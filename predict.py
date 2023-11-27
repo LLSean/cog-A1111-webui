f6349b3696143812bd48c17c9aee579cdb7e6917
fmh <380963653@qq.com>
2023-11-27 18:11:07 +0800

    first commit

---
+++ predict.py
@@ -0,0 +1,134 @@
+# Prediction interface for Cog ⚙️
+# https://github.com/replicate/cog/blob/main/docs/python.md
+
+import os, sys, json
+sys.path.extend(['/stable-diffusion-webui'])
+
+from cog import BasePredictor, BaseModel, Input, Path
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+
+        # workaround for replicate since its entrypoint may contain invalid args
+        os.environ['IGNORE_CMD_ARGS_ERRORS'] = '1'
+        from modules import timer
+        # moved env preparation to build time
+        # from modules import launch_utils
+
+        # with launch_utils.startup_timer.subcategory("prepare environment"):
+        #     launch_utils.prepare_environment()
+
+        from modules import initialize_util
+        from modules import initialize
+
+        startup_timer = timer.startup_timer
+        startup_timer.record("launcher")
+
+        initialize.imports()
+
+        initialize.check_versions()
+
+        initialize.initialize()
+
+        from fastapi import FastAPI
+        app = FastAPI()
+        initialize_util.setup_middleware(app)
+
+        from modules.api.api import Api
+        from modules.call_queue import queue_lock
+        
+        self.api = Api(app, queue_lock)
+
+    def predict(
+        self,
+        prompt: str = Input(description="Prompt"),
+        negative_prompt: str = Input(description="Negative Prompt", default=""),
+        width: int = Input(
+            description="Width of output image", ge=1, le=1024, default=512
+        ),
+        height: int = Input(
+            description="Height of output image", ge=1, le=1024, default=512
+        ),
+        num_outputs: int = Input(
+            description="Number of images to output", ge=1, le=4, default=1
+        ),
+        scheduler: str = Input(
+            description="scheduler",
+            choices=['DPM++ 2M Karras', 'DPM++ SDE Karras', 'DPM++ 2M SDE Exponential', 'DPM++ 2M SDE Karras', 'Euler a', 'Euler', 'LMS', 'Heun', 'DPM2', 'DPM2 a', 'DPM++ 2S a', 'DPM++ 2M', 'DPM++ SDE', 'DPM++ 2M SDE', 'DPM++ 2M SDE Heun', 'DPM++ 2M SDE Heun Karras', 'DPM++ 2M SDE Heun Exponential', 'DPM++ 3M SDE', 'DPM++ 3M SDE Karras', 'DPM++ 3M SDE Exponential', 'DPM fast', 'DPM adaptive', 'LMS Karras', 'DPM2 Karras', 'DPM2 a Karras', 'DPM++ 2S a Karras', 'Restart', 'DDIM', 'PLMS', 'UniPC'],
+            default="DPM++ SDE Karras",
+        ),
+        num_inference_steps: int = Input(
+            description="Number of denoising steps", ge=1, le=100, default=20
+        ),
+        guidance_scale: float = Input(
+            description="Scale for classifier-free guidance", ge=1, le=50, default=7.5
+        ),
+        seed: int = Input(
+            description="Random seed. Leave blank to randomize the seed", default=-1
+        ),
+        # image: Path = Input(description="Grayscale input image"),
+        enable_hr: bool = Input(
+            description="Hires. fix", default=False,
+        ),
+        hr_upscaler: str = Input(
+            description="Upscaler for Hires. fix",
+            choices=['Latent', 'Latent (antialiased)', 'Latent (bicubic)', 'Latent (bicubic antialiased)', 'Latent (nearest)', 'Latent (nearest-exact)', 'None', 'Lanczos', 'Nearest', 'ESRGAN_4x', 'LDSR', 'R-ESRGAN 4x+', 'R-ESRGAN 4x+ Anime6B', 'ScuNET GAN', 'ScuNET PSNR', 'SwinIR 4x'],
+            default="Latent",
+        ),
+        hr_steps: int = Input(
+            description="Inference steps for Hires. fix", ge=0, le=100, default=20
+        ),
+        denoising_strength: float = Input(
+            description="Denoising strength. 1.0 corresponds to full destruction of information in init image", ge=0, le=1, default=0.5
+        ),
+        hr_scale: float = Input(
+            description="Factor to scale image by", ge=1, le=4, default=2
+        ),
+    ) -> list[Path]:
+        """Run a single prediction on the model"""
+        # processed_input = preprocess(image)
+        # output = self.model(processed_image, scale)
+        # return postprocess(output)
+        payload = {
+            # "init_images": [encoded_image],
+            "prompt": prompt,
+            "negative_prompt": negative_prompt,
+            "width": width,
+            "height": height,
+            "batch_size": num_outputs,
+            "steps": num_inference_steps,
+            "cfg_scale": guidance_scale,
+            "seed": seed,
+            "do_not_save_samples": True,
+            "sampler_name": scheduler,
+            "enable_hr": enable_hr,
+            "hr_upscaler": hr_upscaler,
+            "hr_second_pass_steps": hr_steps,
+            "denoising_strength": denoising_strength if enable_hr else None,
+            "hr_scale": hr_scale,
+        }
+
+        from modules.api.models import StableDiffusionTxt2ImgProcessingAPI, StableDiffusionImg2ImgProcessingAPI
+        req = StableDiffusionTxt2ImgProcessingAPI(**payload)
+        # generate
+        resp = self.api.text2imgapi(req)
+        info = json.loads(resp.info)
+
+        from PIL import Image
+        import uuid
+        import base64
+        from io import BytesIO
+
+        outputs = []
+
+        for i, image in enumerate(resp.images):
+            seed = info["all_seeds"][i]
+            gen_bytes = BytesIO(base64.b64decode(image))
+            gen_data = Image.open(gen_bytes)
+            filename = "{}-{}.png".format(seed, uuid.uuid1())
+            gen_data.save(fp=filename, format="PNG")
+            output = Path(filename)
+            outputs.append(output)
+
+        return outputs
